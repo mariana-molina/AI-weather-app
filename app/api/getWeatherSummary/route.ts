418961de4017e6d79337d70ba5bb559d63a59cbe
Mariana <marsmolina20@gmail.com>
2023-08-24 19:41:00 +0200

    change ai connection

--- app/api/getWeatherSummary/route.ts
+++ app/api/getWeatherSummary/route.ts
@@ -5,9 +5,24 @@ export async function POST(request: Request) {
 	const { weatherData } = await request.json();
 
 	const response = await openai.chat.completions.create({
-		messages: [{ role: 'user', content: 'Say this is a test' }],
 		model: 'gpt-3.5-turbo',
+		temperature: 0.8,
+		n: 1,
+		stream: false,
+		messages: [
+			{
+				role: 'system',
+				content:
+					'Pretend you are a weather news presenter LIVE on television be energetic and full of charisma. State the city you are providing a summary for. Then give a summary of todays weather only. Make it easy to understand for the user and list things to prepare (advice) for those weather conditions like use SPF or take an umbrella in 150 words or less',
+			},
+			{
+				role: 'user',
+				content: `hi, can i get a summary of todays weather. Use the following information to get the weather data: ${JSON.stringify(
+					weatherData
+				)}`,
+			},
+		],
 	});
-	console.log('HERE CHATGPT RES:👌👌', response.choices);
+
 	return NextResponse.json(response.choices[0].message);
 }
